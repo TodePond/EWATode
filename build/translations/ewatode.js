7f31e65131b19527faeb3f6c5f0fb03dda6e929d
Luke Wilson <l2wilson94@gmail.com>
2021-03-09 19:17:32 +0000

    spacing

--- build/translations/ewatode.js
+++ build/translations/ewatode.js
@@ -6,7 +6,7 @@
 			const term = (() => {
 	global.EWATode = Term.term('EWATode', scope)
 	return Term.subTerms(Term.string(''), [
-['EWATode', Term.list([
+['EWATode', Term.emit(Term.list([
 		Term.term('EWATode.Magic', scope),
 		Term.term('EWATode.MinorVersion', scope),
 Term.term('EWATode.MajorVersion', scope),
@@ -14,14 +14,18 @@ Term.term('EWATode.CompileTag', scope),
 Term.term('EWATode.Metadata', scope),
 Term.term('EWATode.CodeIndex', scope),
 Term.term('EWATode.Code', scope)
-	])],
+	]), ({output}) => output.match(/.{2}/g).join(" "))],
 ['Magic', Term.chain(Term.emit(Term.string(''), "02 03 07 41"), Term.term('Magic.HexNums', scope))],
 ['MinorVersion', Term.chain(Term.emit(Term.string(''), "00 01"), Term.term('MinorVersion.HexNums', scope))],
 ['MajorVersion', Term.chain(Term.emit(Term.string(''), "00 00"), Term.term('MajorVersion.HexNums', scope))],
 ['CompileTag', Term.chain(Term.emit(Term.string(''), "53 41 4e 44 50 4f 4e 44"), Term.term('CompileTag.HexNums', scope))],
-['HexNums', Term.many(Term.term('HexNums.HexNum', scope))],
-['HexNum', Term.list([Term.term('HexNum.HexNumDigit', scope), Term.string(` `), Term.term('HexNum.HexNumDigit', scope)])],
+['HexNums', Term.chain(Term.term('HexNums.Strip', scope), Term.many(Term.term('HexNums.HexNum', scope)))],
+['HexNum', Term.list([Term.term('HexNum.HexNumDigit', scope), Term.term('HexNum.HexNumDigit', scope)])],
 ['HexNumDigit', Term.regExp(/[0-9A-Fa-f]/)],
+['Strip', Term.maybe(Term.many(Term.or([
+		Term.emit(Term.string(` `), ""),
+		Term.regExp(/[^]/)
+	])))],
 ['Metadata', Term.chain(Term.emit(Term.string(''), "00"), Term.term('Metadata.HexNums', scope))],
 ['CodeIndex', Term.chain(Term.emit(Term.string(''), "00 00"), Term.term('CodeIndex.HexNums', scope))],
 ['Code', Term.chain(Term.emit(Term.string(''), "00 00"), Term.term('Code.HexNums', scope))]
--- source/ewatode.mt
+++ source/ewatode.mt
@@ -9,16 +9,21 @@
 		Metadata
 		CodeIndex
 		Code
-	)
+	) >> ({output}) => output.match(/.{2}/g).join(" ")
 	
 	Magic :: HexNums ++ >> "02 03 07 41"
 	MinorVersion :: HexNums ++ >> "00 01"
 	MajorVersion :: HexNums ++ >> "00 00"
 	CompileTag :: HexNums ++ >> "53 41 4e 44 50 4f 4e 44"
 	
-	HexNums :: HexNum+
-	HexNum :: HexNumDigit " " HexNumDigit
+	HexNums ++ Strip :: HexNum+
+	HexNum :: HexNumDigit HexNumDigit
 	HexNumDigit :: /[0-9A-Fa-f]/
+	
+	Strip :: {<
+		:: " " >> ""
+		/[^]/
+	>}
 	
 	Metadata :: HexNums ++ >> "00"
 	CodeIndex :: HexNums ++ >> "00 00"
